48ec48963311de4b6b7138374326add737af4bb8
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-01-20 15:08:39 +0530

    done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,11 @@
-import { Box } from '@mui/material'
+import { Box, useMediaQuery, useTheme } from '@mui/material'
+import TypingAnim from '../components/Typer/TypingAnim'
+import Footer from '../components/footer/Footer'
 
 
 function Home() {
+  const theme = useTheme();
+  const isBelowMd = useMediaQuery(theme.breakpoints.down("md"))
   return (
     <Box width={'100%'} height={'100%'}>
       <Box 
@@ -9,8 +13,21 @@ function Home() {
         width:"100%",
         flexDirection:'column',
         alignItems:"center",
-        mx:'auto'
-      }}></Box>
+        mx:'auto',
+        mt:3,
+      }}>
+        <Box>
+          <TypingAnim/>
+        </Box>
+        <Box sx={{width:'100%', display:'flex',flexDirection:{md:"row", xs:'column',sm:'columan'},gap:5,my:10}}>
+          <img src="robot.png" alt="" style={{width:'200px', margin:'auto'}}/>
+          <img className='image-inverted rotate' src="openai.png" alt="" style={{width:'200px', margin:'auto'}}/>
+        </Box>
+        <Box sx={{display:'flex', width:'100%', mx:"auto"}}>
+          <img src="chat.png" alt="" style={{display:"flex",margin:'auto',width: isBelowMd ? '80%' : '60%',borderRadius:20, boxShadow:'-5px -5px 105px #64f3d5', marginTop:20, marginBottom:20, }} />
+        </Box>
+      </Box>
+      <Footer/>
     </Box>
   )
 }
